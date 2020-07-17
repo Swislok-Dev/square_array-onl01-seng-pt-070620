bd0b1b09cdb9988923041efcfdc928d1f0306a72
Cory Marker <cmarker4@gmail.com>
2020-07-17 14:16:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = Array.new
   array.each do |number|
     squared_number = number * number
-    new_array.push(squared_number)
+    new_array.collect(squared_number)
 
   end
   new_array
